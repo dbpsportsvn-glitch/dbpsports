f54eb1873cf83fd13909e5db2a5a7b293faeab39
Cong Nguyen <dbpsportsvn@gmail.com>
2025-08-27 18:54:36 +0700

    Cập nhật trang ADmin

--- backend/tournaments/admin.py
+++ backend/tournaments/admin.py
@@ -1,10 +1,27 @@
 # tournaments/admin.py
 
 from django.contrib import admin
-from .models import Tournament, Team, Player, Match # Thêm Match vào đây
+from .models import Tournament, Team, Player, Match
 
-# Đăng ký các model
-admin.site.register(Tournament)
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(Match) # Thêm dòng này
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date')
+    search_fields = ('name',)
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tournament', 'captain', 'coach_name')
+    list_filter = ('tournament',)
+    # Đưa trường tìm được (username) lên trước, sau đó thêm các trường khác
+    search_fields = ('captain__username', 'name', 'coach_name',)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'team', 'jersey_number', 'position')
+    list_filter = ('team__tournament', 'team')
+    search_fields = ('full_name',)
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'tournament', 'match_time', 'location')
+    list_filter = ('tournament',)
